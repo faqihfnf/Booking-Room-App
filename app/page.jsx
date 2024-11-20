f7b92a812650ab098f9788abed597b24bec67ad0
faqihfnf <faqih.fnf@gmail.com>
2024-11-20 19:59:12 +0700

    add dummy data room

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import RoomCard from "@/components/RoomCard";
+import rooms from "../data/rooms.json";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms available</p>
+      )}
     </>
   );
 }
